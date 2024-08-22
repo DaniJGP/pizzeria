6cd87c6fa6fd9ea74aa81c9b2c44a125e8ae70de
Daniela Guardia <138531935+DaniJGP@users.noreply.github.com>
2024-08-22 15:40:29 -0400

    Replace pizzas.js array with API call in Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,24 @@
 import CardPizza from './CardPizza';
-import { pizzas } from '../assets/pizzas';
+import { useState, useEffect } from 'react';
 import './Home.css';
 const Home = () => {
+    // State que alojará el array de objetos de pizzas
+    const [pizzas, setPizzas] = useState([]);
+
+    // URL del endpoint de la API
+    const pizzasURL = 'http://localhost:5000/api/pizzas';
+
+    // Función asíncrona que actualiza el state
+    const callPizzasAPI = async () => {
+        const res = await fetch(pizzasURL);
+        const data = await res.json();
+        setPizzas(data);
+    };
+
+    useEffect(() => {
+        callPizzasAPI();
+    }, []);
+
     return (
         <>
             <main id="mainStore" className="py-4 px-2">
